35e7f31df1ca8c74d387441ca4c42478977281b2
Phong Lo <phonglo724@gmail.com>
2021-04-07 11:06:16 -0600

    react components

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <h1>Happy App Fun Time with MeerKats, Cats, and Dogs!!!</h1>
+            </header>
+        )
+    }
+}
+
+// THE ONLY RULE USING RCC IS RETURNING ONE HTML ELEMENT.
+// IF YOU WANT TO USE STATE IN THIS FILE, THIS CLASS COMPONENT WILL USE "THIS.".
